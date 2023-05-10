aa6042e470366f920e1b12894b5026a448c192d4
Adam Bartonicek <bartonicek@gmail.com>
2023-05-10 18:52:18 +1200

    Made some changes

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,13 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
+import html from "solid-js/html";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const app = document.querySelector<HTMLDivElement>("#app")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const heading = html`<h1>Adam Bartonicek</h1>`;
+app.appendChild(heading);
+
+const blurb = html`<p />`;
+blurb.innerText =
+  "Hi, my name is Adam. I am interested in data science and visualization";
+
+app.appendChild(blurb);
